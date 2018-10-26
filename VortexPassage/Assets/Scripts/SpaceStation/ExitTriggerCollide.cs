9abded65fbe310def16a1dfb4cc57c25a333caf9
HackNJamSpace <bolillo.duro@live.com>
2018-10-26 17:46:26 -0500

    Added animations for the first scene.
    Added animation, lacks transistion.

---
+++ VortexPassage/Assets/Scripts/SpaceStation/ExitTriggerCollide.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitTriggerCollide : MonoBehaviour {
+
+    public SpaceStationGameController spaceController;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            spaceController.crash = true;
+        }
+    }
+}
---
+++ VortexPassage/Assets/Scripts/SpaceStation/SpaceStationGameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceStationGameController : MonoBehaviour {
+
+    public bool emergency, crash;
+    public GameObject emergencyLights;
+    public GameObject stationObject;
+
+
+	// Use this for initialization
+	void Start () {
+        emergency = false;
+        crash = false;
+        emergencyLights.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (emergency)
+        {
+            emergencyLights.SetActive(true);
+            stationObject.GetComponent<Animator>().SetTrigger("crashed");
+        }
+
+        if (crash)
+        {
+           
+        }
+	}
+}
