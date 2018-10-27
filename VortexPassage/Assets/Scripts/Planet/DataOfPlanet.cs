d8909aab5d6880fd6269f3e09e269610d0b40f0e
HackNJamSpace <bolillo.duro@live.com>
2018-10-26 23:24:29 -0500

    Added description script.

---
+++ VortexPassage/Assets/Scripts/Planet/DataOfPlanet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataOfPlanet : MonoBehaviour {
+
+    GameObject canvas;
+    DisplayData displayData;
+
+
+    public string title;
+    [TextArea(15, 20)]
+    public string description;
+    // Use this for initialization
+    void Start () {
+        canvas = GameObject.FindGameObjectWithTag("CanvasDataDisplay");
+        displayData = canvas.GetComponent<DisplayData>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            displayData.title.text = title;
+            displayData.description.text = description;
+            displayData.showCanvas = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            displayData.title.text = "";
+            displayData.description.text = "";
+            displayData.showCanvas = false;
+        }
+    }
+}
---
+++ VortexPassage/Assets/Scripts/Planet/DisplayData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayData : MonoBehaviour {
+
+	public Text title, description;
+    public bool showCanvas;
+    Animator anim;
+    public GameObject infoCanvas;
+
+    void Start()
+    {
+        showCanvas = false;
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        anim.SetBool("open", showCanvas);
+    }
+
+}
